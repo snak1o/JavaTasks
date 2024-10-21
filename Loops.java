07bda45ee0192f8ed0933c152d7e56de84e2e50e
snak1o <64849331+snak1o@users.noreply.github.com>
2024-10-21 22:19:45 +0300

    Add Loops task code

---
+++ Loops.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many rows?");
+        String count = sc.nextLine();
+        for (int rows = 1; rows <= Integer.parseInt(count); rows++) {
+            for (int spaces = 0; spaces < Integer.parseInt(count) - rows; spaces++) {
+
+                System.out.print(" ");
+            }
+            for (int stars = 1; stars <= rows; stars++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
